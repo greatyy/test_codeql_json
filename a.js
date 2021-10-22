836b6bfdd0d41823dd39a896a2e09d846b2aa7fb
greatyy <48902334+greatyy@users.noreply.github.com>
2021-10-22 09:32:15 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,12 @@
-console.log("aaaa")
+var a = request.body;
+var obj = {
+    "body": a.a,
+    "headers": a.b
+}
+function buildMsg(obj){
+   let result = new SomeKindOfClass();
+   result.body = obj.body;
+   result.headers = obj.headers
+   return result
+}
+foo(buildMsg(obj))
